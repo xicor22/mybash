0f438dab01b446863250ae3dffce2bc0629911aa
Saksham Singh <sakshamsingh.93502@gmail.com>
2024-09-15 17:14:14 +0530

    stuff

--- setup.sh
+++ setup.sh
@@ -37,105 +37,15 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-checkEnv() {
-    ## Check for requirements.
-    REQUIREMENTS='curl groups sudo'
-    for req in $REQUIREMENTS; do
-        if ! command_exists "$req"; then
-            echo "${RED}To run me, you need: $REQUIREMENTS${RC}"
-            exit 1
-        fi
-    done
-
-    ## Check Package Handler
-    PACKAGEMANAGER='nala apt dnf yum pacman zypper emerge xbps-install nix-env'
-    for pgm in $PACKAGEMANAGER; do
-        if command_exists "$pgm"; then
-            PACKAGER="$pgm"
-            echo "Using $pgm"
-            break
-        fi
-    done
-
-    if [ -z "$PACKAGER" ]; then
-        echo "${RED}Can't find a supported package manager${RC}"
-        exit 1
-    fi
-
-    if command_exists sudo; then
-        SUDO_CMD="sudo"
-    elif command_exists doas && [ -f "/etc/doas.conf" ]; then
-        SUDO_CMD="doas"
-    else
-        SUDO_CMD="su -c"
-    fi
-
-    echo "Using $SUDO_CMD as privilege escalation software"
-
-    ## Check if the current directory is writable.
-    GITPATH=$(dirname "$(realpath "$0")")
-    if [ ! -w "$GITPATH" ]; then
-        echo "${RED}Can't write to $GITPATH${RC}"
-        exit 1
-    fi
-
-    ## Check SuperUser Group
-
-    SUPERUSERGROUP='wheel sudo root'
-    for sug in $SUPERUSERGROUP; do
-        if groups | grep -q "$sug"; then
-            SUGROUP="$sug"
-            echo "Super user group $SUGROUP"
-            break
-        fi
-    done
-
-    ## Check if member of the sudo group.
-    if ! groups | grep -q "$SUGROUP"; then
-        echo "${RED}You need to be a member of the sudo group to run me!${RC}"
-        exit 1
-    fi
-}
-
 installDepend() {
     ## Check for dependencies.
-    DEPENDENCIES='bash bash-completion tar bat tree multitail fastfetch wget unzip fontconfig'
+    DEPENDENCIES='bash bash-completion tar bat tree multitail wget unzip fontconfig'
     if ! command_exists nvim; then
         DEPENDENCIES="${DEPENDENCIES} neovim"
     fi
 
     echo "${YELLOW}Installing dependencies...${RC}"
-    if [ "$PACKAGER" = "pacman" ]; then
-        if ! command_exists yay && ! command_exists paru; then
-            echo "Installing yay as AUR helper..."
-            ${SUDO_CMD} ${PACKAGER} --noconfirm -S base-devel
-            cd /opt && ${SUDO_CMD} git clone https://aur.archlinux.org/yay-git.git && ${SUDO_CMD} chown -R "${USER}:${USER}" ./yay-git
-            cd yay-git && makepkg --noconfirm -si
-        else
-            echo "AUR helper already installed"
-        fi
-        if command_exists yay; then
-            AUR_HELPER="yay"
-        elif command_exists paru; then
-            AUR_HELPER="paru"
-        else
-            echo "No AUR helper found. Please install yay or paru."
-            exit 1
-        fi
-        ${AUR_HELPER} --noconfirm -S ${DEPENDENCIES}
-    elif [ "$PACKAGER" = "nala" ]; then
-        ${SUDO_CMD} ${PACKAGER} install -y ${DEPENDENCIES}
-    elif [ "$PACKAGER" = "emerge" ]; then
-        ${SUDO_CMD} ${PACKAGER} -v app-shells/bash app-shells/bash-completion app-arch/tar app-editors/neovim sys-apps/bat app-text/tree app-text/multitail app-misc/fastfetch
-    elif [ "$PACKAGER" = "xbps-install" ]; then
-        ${SUDO_CMD} ${PACKAGER} -v ${DEPENDENCIES}
-    elif [ "$PACKAGER" = "nix-env" ]; then
-        ${SUDO_CMD} ${PACKAGER} -iA nixos.bash nixos.bash-completion nixos.gnutar nixos.neovim nixos.bat nixos.tree nixos.multitail nixos.fastfetch  nixos.pkgs.starship
-    elif [ "$PACKAGER" = "dnf" ]; then
-        ${SUDO_CMD} ${PACKAGER} install -y ${DEPENDENCIES}
-    else
-        ${SUDO_CMD} ${PACKAGER} install -yq ${DEPENDENCIES}
-    fi
+    ${SUDO_CMD} ${PACKAGER} install -yq ${DEPENDENCIES}
 
     # Check to see if the JetBrains Mono Nerd Font is installed (Change this to whatever font you would like)
     FONT_NAME="JetBrainsMono Nerd Font"
@@ -164,16 +74,7 @@ installDepend() {
     fi
 }
 
-installStarshipAndFzf() {
-    if command_exists starship; then
-        echo "Starship already installed"
-        return
-    fi
-
-    if ! curl -sS https://starship.rs/install.sh | sh; then
-        echo "${RED}Something went wrong during starship install!${RC}"
-        exit 1
-    fi
+installFzf() {
     if command_exists fzf; then
         echo "Fzf already installed"
     else
@@ -194,41 +95,10 @@ installZoxide() {
     fi
 }
 
-install_additional_dependencies() {
-    # we have PACKAGER so just use it
-    # for now just going to return early as we have already installed neovim in `installDepend`
-    # so I am not sure why we are trying to install it again
-    return
-   case "$PACKAGER" in
-        *apt)
-            if [ ! -d "/opt/neovim" ]; then
-                curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-                chmod u+x nvim.appimage
-                ./nvim.appimage --appimage-extract
-                ${SUDO_CMD} mv squashfs-root /opt/neovim
-                ${SUDO_CMD} ln -s /opt/neovim/AppRun /usr/bin/nvim
-            fi
-            ;;
-        *zypper)
-            ${SUDO_CMD} zypper refresh
-            ${SUDO_CMD} zypper -n install neovim # -y doesn't work on opensuse -n is short for -non-interactive which is equivalent to -y
-            ;;
-        *dnf)
-            ${SUDO_CMD} dnf check-update
-            ${SUDO_CMD} dnf install -y neovim
-            ;;
-        *pacman)
-            ${SUDO_CMD} pacman -Syu
-            ${SUDO_CMD} pacman -S --noconfirm neovim
-            ;;
-        *)
-            echo "No supported package manager found. Please install neovim manually."
-            exit 1
-            ;;
-    esac
-}
-
 create_fastfetch_config() {
+    wget https://github.com/fastfetch-cli/fastfetch/releases/latest/download/fastfetch-linux-aarch64.deb >> /dev/null 2>&1
+    sudo dpkg -i fastfetch-linux-aarch64.deb
+    rm fastfetch-linux-aarch64.deb
     ## Get the correct user home directory.
     USER_HOME=$(getent passwd "${SUDO_USER:-$USER}" | cut -d: -f6)
     
@@ -263,17 +133,12 @@ linkConfig() {
         echo "${RED}Failed to create symbolic link for .bashrc${RC}"
         exit 1
     }
-    ln -svf "$GITPATH/starship.toml" "$USER_HOME/.config/starship.toml" || {
-        echo "${RED}Failed to create symbolic link for starship.toml${RC}"
-        exit 1
-    }
 }
 
-checkEnv
+
 installDepend
-installStarshipAndFzf
+installFzf
 installZoxide
-install_additional_dependencies
 create_fastfetch_config
 
 if linkConfig; then
